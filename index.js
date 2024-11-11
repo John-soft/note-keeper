54b0d48c81a9379e8d26cf06cd07b4e05fa7793e
John-soft <akinwalere52@gmail.com>
2024-11-11 17:25:13 +0100

    error handling fixes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const morgan = require("morgan");
 const http = require("http");
 const bodyParser = require("body-parser");
+const CustomError = require("./utils/custom.error");
 const connectDB = require("./config/db");
 const AppRoutes = require("./routes/app.routes");
 const server = http.createServer(app);
@@ -17,7 +18,56 @@ app.get("/", (req, res) => {
 });
 app.use("/api", AppRoutes);
 
+app.all("*", (req, res, next) => {
+  const err = new CustomError(`Route ${req.originalUrl} not found`, 404);
+  next(err);
+});
+
 server.listen(PORT, () => {
   connectDB();
   console.log(`Server running on port ${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("Unhandled rejection occured! Shutting down");
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+//Handling uncaught exceptions
+//Exceptions are synchronous errors in the program **** also shuts down the server
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("Uncaught exception occured! Shutting down");
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+//Signals for closing the server in case of any error
+
+// Triggered by: Pressing Ctrl + C in the terminal.
+// Default behavior: Terminates the process.
+// Usage: You might handle SIGINT to perform cleanup tasks before the process shuts down.
+process.on("SIGINT", () => {
+  console.log("SIGINT signal received");
+  process.exit(1);
+});
+
+// Triggered by: This is the standard signal used to politely ask a process to terminate. Often sent by kill <PID>, Docker, Kubernetes, or systemd when shutting down services.
+// Default behavior: Terminates the process.
+// Usage: Commonly used in production environments to gracefully shut down the process.
+process.on("SIGTERM", () => {
+  console.log("SIGTERM signal received");
+  process.exit(1);
+});
+
+// Triggered by: User pressing Ctrl + \ in the terminal. It’s similar to SIGINT but may also trigger a core dump (a file that records the memory of the process for debugging).
+// Default behavior: Terminates the process and may generate a core dump.
+// Usage: You may want to catch this signal to log additional debugging information before exiting.
+process.on("SIGQUIT", () => {
+  console.log("SIGQUIT signal received");
+  process.exit(1);
 });
