aa13b389f4fc2dc7c28507dbc30626a89f34964a
John-soft <akinwalere52@gmail.com>
2024-11-11 09:59:41 +0100

    completed the crud operations

--- controllers/note.controller.js
+++ controllers/note.controller.js
@@ -11,8 +11,112 @@ class NoteController {
           data: note,
           message: "A new note created successfully",
         },
+        201
+      );
+    } catch (error) {
+      handleResponse(
+        req,
+        res,
+        { message: "An error occured, please try again!" },
+        500
+      );
+    }
+  };
+
+  viewAllNotes = async (req, res) => {
+    try {
+      const notes = await Note.find();
+      if (!notes) {
+        return {
+          message: "Notes not found",
+        };
+      }
+      handleResponse(
+        req,
+        res,
+        {
+          notes,
+          message: "All notes retrieved successfully!!!",
+        },
         200
       );
+    } catch (error) {
+      handleResponse(
+        req,
+        res,
+        { message: "An error occured, please try again!" },
+        500
+      );
+    }
+  };
+
+  viewNote = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const notes = await Note.findOne({ id });
+      if (!notes) {
+        return {
+          message: "Note not found",
+        };
+      }
+      handleResponse(
+        req,
+        res,
+        {
+          notes,
+          message: "Note retrieved successfully!!!",
+        },
+        200
+      );
+    } catch (error) {
+      handleResponse(
+        req,
+        res,
+        { message: "An error occured, please try again!" },
+        500
+      );
+    }
+  };
+
+  editNote = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const note = await Note.findByIdAndUpdate({ id }, req.body, {
+        new: true,
+      });
+      if (!note) {
+        return {
+          message: "Note not found",
+        };
+      }
+
+      handleResponse(
+        req,
+        res,
+        { data: note, message: "Note updated successfully " },
+        200
+      );
+    } catch (error) {
+      handleResponse(
+        req,
+        res,
+        { message: "An error occured, please try again!" },
+        500
+      );
+    }
+  };
+
+  deleteNote = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const note = await Note.findByIdAndDelete({ id });
+      if (!note) {
+        return {
+          message: "Note not found",
+        };
+      }
+
+      handleResponse(req, res, { data: null, message: "Note deleted" }, 204);
     } catch (error) {
       handleResponse(
         req,
--- routes/note.routes.js
+++ routes/note.routes.js
@@ -1,6 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const AuthMiddleware = require("../middleware/auth.middleware");
 const NoteController = require("../controllers/note.controller");
-router.post("/create-note", NoteController.createNote);
+router.post(
+  "/create-note",
+  AuthMiddleware.validateBearerToken,
+  NoteController.createNote
+);
+router.get(
+  "/view-notes",
+  AuthMiddleware.validateBearerToken,
+  NoteController.viewAllNotes
+);
+
+router.get(
+  "/view-note/:id",
+  AuthMiddleware.validateBearerToken,
+  NoteController.viewNote
+);
+
+router.patch(
+  "/edit-note/:id",
+  AuthMiddleware.validateBearerToken,
+  NoteController.editNote
+);
+
+router.delete(
+  "/delete-note/:id",
+  AuthMiddleware.validateBearerToken,
+  NoteController.deleteNote
+);
 
 module.exports = router;
